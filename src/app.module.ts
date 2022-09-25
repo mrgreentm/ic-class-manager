6bfce63a2c4fbd32eef731207feea7a78b1d863c
Joao Souza <joaonetotfm@gmail.com>
2022-09-24 21:42:56 -0300

    feature: implements typeorm and mysql integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: +process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME || 'root',
+      database: process.env.TYPEORM_DATABASE || 'ic_class_manager',
+      password: process.env.TYPEORM_PASSWORD || 'nikolatesla06@',
+      entities: [],
+      synchronize: false,
+      logging: false,
+      migrations: ['database/migrations/**/*.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
